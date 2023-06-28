3e72bea880f933dd304439ded8e5cba810288928
Evgenii Pasechnikov <evgenii.pase4nickov@gmail.com>
2023-06-28 19:19:02 +0300

    Homework 27.06

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,37 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = Math.pow(b, 2) - 4 * (a * c);
+  let root1;
+  let root2;
+  if (discriminant < 0) {
+    return arr;
+  } else if (discriminant == 0) {
+    root1 = -b / (2 * a);
+    arr.push(root1);
+  } else if (discriminant > 0) {
+    root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    arr.push(root1, root2);
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let interestRate = percent / 100 / 12;
+
+  let bodyCredit = amount - contribution;
+
+  let payMonth =
+    bodyCredit *
+    (interestRate + interestRate / ((1 + interestRate) ** countMonths - 1));
+
+  let totalSum = peyMonth * countMonths;
+
+  totalSum = totalSum.toFixed(2);
+
+  totalSum = Number(totalSum);
+
+  return;
 }
