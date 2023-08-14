6ec3789b6df1000547b272f303f346b4d34d4695
Evgenii Pasechnikov <evgenii.pase4nickov@gmail.com>
2023-08-14 16:55:38 +0300

    Homework 14.08

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿function parseCount(value) {
+  if (Number.parseFloat(value) == NaN) {
+    throw new Error("Невалидное значение");
+  }
+  return Number.parseFloat(value);
+}
+
+function validateCount(value) {
+  try {
+    return parseCount(value);
+  } catch (error) {
+    return error;
+  }
+}
+
+// Задание №2
+
+class Triangle {
+  constructor(a, b, c) {
+    if (a + b < c || a + c < b || b + c < a) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+  }
+
+  get perimeter() {
+    let p = a + b + c;
+    return p;
+  }
+
+  get area() {
+    let s = Math.sqrt((a + b + c) ** 2 - 2 * (a ** 4 + b ** 4 + c ** 4)) / 4;
+    return s;
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    let newTriangle = new Triangle(a, b, c);
+    return newTriangle;
+  } catch (error) {
+    return {
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      },
+
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";
+      },
+    };
+  }
+}
