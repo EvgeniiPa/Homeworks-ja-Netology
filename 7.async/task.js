f927e0485c527ee8f4bc34ada77646f87adbc325
Evgenii Pasechnikov <evgenii.pase4nickov@gmail.com>
2023-08-24 10:50:25 +0300

    Homework 24.08

--- 7.async/task.js
+++ 7.async/task.js
@@ -46,7 +46,7 @@ class AlarmClock {
           item.canCall == true
         ) {
           item.canCall = false;
-          item.callback;
+          item.callback();
         }
       });
     }, 1000);
